df67f7ab0e8b812aa893729f059e6615c4214be0
labiker <labiker2233@gmail.com>
2023-10-21 16:58:36 +0900

    Add: fonts, images and sound effects assets necessary for designing the title scene.

--- src/utils/assets.ts
+++ src/utils/assets.ts
@@ -1,4 +1,6 @@
 import { sound } from '@pixi/sound';
+import { image } from '../core/image';
+import { Assets } from 'pixi.js';
 
 /** Initialise and start background loading of all assets */
 export async function initAssets() {
@@ -6,4 +8,13 @@ export async function initAssets() {
     sound.add('primary-button-press', 'app://assets/audio/primary-button-press.wav');
     sound.add('sightless-storm-ii', 'app://assets/audio/sightless-storm-ii.mp3');
     sound.add('sfx_print', 'app://assets/audio/sfx_print.mp3');
+
+    // Load image assets
+    await image.add('play-btn-up', 'app://assets/images/play-btn-up.png');
+    await image.add('play-btn-down', 'app://assets/images/play-btn-down.png');
+    await image.add('player', 'app://assets/images/player.png');
+
+    // Load font assets
+    await Assets.load('app://assets/fonts/Ma_Shan_Zheng/Ma_Shan_Zheng.ttf');
+    await Assets.load('app://assets/fonts/ZCOOL_XiaoWei/ZCOOL_XiaoWei.ttf');
 }
