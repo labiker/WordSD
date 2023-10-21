f525ed1eaf93033d7a7cf501d7e1ecc1138e5351
labiker <labiker2233@gmail.com>
2023-10-21 16:48:03 +0900

    Add: 'i18n' utility.

---
+++ src/utils/i18n-en.ts
@@ -0,0 +1,25 @@
+/**
+ * A dictionary of strings used in-game
+ */
+export const i18nDict = {
+    pixiLogoHeader: 'Made with',
+    gameTitleTop: '租  房',
+    gameTitleBottom: 'RENTING',
+    playButton: '入  住\nCheck in',
+    forkGithub: 'Fork me\non Github',
+    helperMobile: 'Hold to aim\nRelease to shoot!',
+    helperDesktop: 'Move mouse to aim\nLeft click to shoot!',
+    paused: 'GAME PAUSED',
+    score: 'CURRENT SCORE',
+    sound: 'Sound',
+    resume: 'Resume',
+    quit: 'Quit',
+    resultsPlay: 'BUB AGAIN',
+    resultsTitle: 'YOUR SCORE!',
+    resultsBreakdownTitle: 'BUB BREAKDOWN',
+    resultsBreakdownPopped: 'Bubbles popped',
+    resultsBreakdownPowerups: 'Powerups used',
+    resultsBreakdownCombo: 'Best combo',
+    resultsScoreTitle: 'FINAL SCORE',
+    resultsHighscoreTitle: 'HIGHSCORE',
+};
